39eb84777fd01555aa7d1c790b5de1662979238f
irinakolbun <kolbunirina01@gmail.com>
2020-04-16 14:33:34 +0300

    Reverse audio.

--- src/com/company/WaveFile.java
+++ src/com/company/WaveFile.java
@@ -2,11 +2,13 @@ package com.company;
 
 import java.io.FileOutputStream;
 import java.nio.ByteBuffer;
-import java.nio.ByteOrder;;
+import java.nio.ByteOrder;
+
 
 public class WaveFile {
     int[] headerInt;
     short[] dataShort;
+    private double kLength;
 
     WaveFile(String path) {
         getParameters(path);
@@ -21,26 +23,40 @@ public class WaveFile {
 
     void changeAudioLength(double kLength) {
         int size = headerInt[headerInt.length - 1];
-        int newSize = (int) Math.round(size * kLength);
+        int newSize = Math.abs((int) Math.round(size * kLength));
         headerInt[headerInt.length - 1] = newSize;
         headerInt[1] = 36 + newSize;
 
 
-
         double[] newArrayX = new double[dataShort.length];
+
         for (int i = 0; i < newArrayX.length; i++) {
-            newArrayX[i] = i * kLength;
+            newArrayX[i] = i * Math.abs(kLength);
+
         }
 
-        int newLength = (int) Math.round(dataShort.length*kLength);
+        if (kLength < 0) {
+            for (int i = 0; i < newArrayX.length / 2; i++) {
+                short temp = dataShort[i];
+                dataShort[i] = dataShort[newArrayX.length - i - 1];
+                dataShort[newArrayX.length - i - 1] = temp;
+            }
+        }
+
+
+        int newLength = Math.abs((int) Math.round(dataShort.length * kLength));
+
         short[] newData = getInterpolatedData(newArrayX, newLength);
+
         dataShort = newData;
+
 
     }
 
     private short[] getInterpolatedData(double[] newArrayX, int newLength) {
         short[] newArrayY = new short[newLength];
         int index = 0;
+
         for (int x = 0; x < newLength; x++) {
 
             while (index < newArrayX.length && newArrayX[index] < x) {
@@ -48,12 +64,24 @@ public class WaveFile {
             }
 
             if (index >= newArrayX.length - 1) {
-                newArrayY[x] = dataShort[dataShort.length-1];
+                newArrayY[x] = dataShort[dataShort.length - 1];
                 continue;
             }
 
             if (newArrayX[index] == x) {
                 newArrayY[x] = dataShort[index];
+                continue;
+            }
+
+            if (index == 0) {
+
+                newArrayY[x] = dataShort[dataShort.length - 1];
+                continue;
+            }
+
+            if (index == dataShort.length - 1) {
+
+                newArrayY[x] = dataShort[0];
                 continue;
             }
 
@@ -71,14 +99,15 @@ public class WaveFile {
         return newArrayY;
     }
 
-    void fileRecording(String pathOutput){
-        int fileLength = headerInt.length*4 + dataShort.length*2;
+    void fileRecording(String pathOutput) {
+
+        int fileLength = headerInt.length * 4 + dataShort.length * 2;
         ByteBuffer outBuf = ByteBuffer.allocate(fileLength);
         outBuf.order(ByteOrder.LITTLE_ENDIAN);
-        for(int i = 0; i<headerInt.length; i++){
+        for (int i = 0; i < headerInt.length; i++) {
             outBuf.putInt(headerInt[i]);
         }
-        for(int i = 0; i<dataShort.length; i++){
+        for (int i = 0; i < dataShort.length; i++) {
             outBuf.putShort(dataShort[i]);
         }
         try {
